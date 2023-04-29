c0e3a95a720866f9cfd1a2ca07d198308f49d383
monica <mon19940513@gmail.com>
2023-04-29 15:05:20 +0800

    Add 3D Graph

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
-          <p className="text-white text-[18px] font-bold cursor-pointer">Monica <span className="sm:block hidden">| Javascript Mastery</span></p>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Monica &nbsp;
+            <span className="sm:block hidden"> | Javascript Mastery</span></p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
